30b42a9fa3666b1433548e7d3652f436bf1ee183
Justin Peters <JMarvel525@gmail.com>
2022-07-22 14:29:46 -0700

    add test to test constructor if it will create and instance of order, test passes

--- VendorTracker.Tests/ModelTests/OrderTests.cs
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -2,3 +2,23 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VendorTracker.Models;
 using System.Collections.Generic;
 using System;
+
+namespace VendorTracker.Tests
+{
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+    public void Dispose()
+    {
+      // Order.ClearAll();
+    }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceofOrder_Order()
+    {
+      Order newOrder = new Order("test");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+
+  }
+}
