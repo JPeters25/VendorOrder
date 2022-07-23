0157bcd15e9ca52a23863f9eaec6a4beb7759bda
Justin Peters <JMarvel525@gmail.com>
2022-07-22 17:01:15 -0700

    add html. Final commit before submission

--- VendorTracker/Controllers/HomeController.cs
+++ VendorTracker/Controllers/HomeController.cs
@@ -10,11 +10,5 @@ namespace VendorTracker.Controllers
       {
         return View();
       }
-
-      [Route("/favorite_photos")]
-      public ActionResult FavoritePhotos()
-      {
-        return View();
-      }
     }
 }
